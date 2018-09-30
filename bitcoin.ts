4be1ed6e4a3ce984ac82cbe1497c9f792ecc6b5e
Chen-Mou Cheng <chenmou.cheng@gmail.com>
2018-09-30 07:53:30 +0000

    adding support for SegWit P2SH-P2WPKH

--- bitcoin.ts
+++ bitcoin.ts
@@ -26,6 +26,9 @@ namespace Utils {
     return [y,x];
   };
 };
+let ripemd = (buf : Buffer) : Buffer => new      ripemd160().update(buf).digest();
+let sha256 = (buf : Buffer) : Buffer => createHash('sha256').update(buf).digest();
+let sha1   = (buf : Buffer) : Buffer => createHash('sha1')  .update(buf).digest();
 
 /*
  * Script parsing and assembling
@@ -33,6 +36,7 @@ namespace Utils {
 namespace Script {
   // https://en.bitcoin.it/wiki/Script
   let opcodes = ['OP_1NEGATE', 'OP_0', 'OP_1', 'OP_2', 'OP_3', 'OP_4', 'OP_5', 'OP_6', 'OP_7', 'OP_8', 'OP_9', 'OP_10', 'OP_11', 'OP_12', 'OP_13', 'OP_14', 'OP_15', 'OP_16', 'OP_NOP', 'OP_VER', 'OP_IF', 'OP_NOTIF', 'OP_VERIF', 'OP_VERNOTIF', 'OP_ELSE', 'OP_ENDIF', 'OP_VERIFY', 'OP_RETURN', 'OP_TOALTSTACK', 'OP_FROMALTSTACK', 'OP_2DROP', 'OP_2DUP', 'OP_3DUP', 'OP_2OVER', 'OP_2ROT', 'OP_2SWAP', 'OP_IFDUP', 'OP_DEPTH', 'OP_DROP', 'OP_DUP', 'OP_NIP', 'OP_OVER', 'OP_PICK', 'OP_ROLL', 'OP_ROT', 'OP_SWAP', 'OP_TUCK', 'OP_CAT', 'OP_SUBSTR', 'OP_LEFT', 'OP_RIGHT', 'OP_SIZE', 'OP_INVERT', 'OP_AND', 'OP_OR', 'OP_XOR', 'OP_EQUAL', 'OP_EQUALVERIFY', 'OP_RESERVED1', 'OP_RESERVED2', 'OP_1ADD', 'OP_1SUB', 'OP_2MUL', 'OP_2DIV', 'OP_NEGATE', 'OP_ABS', 'OP_NOT', 'OP_0NOTEQUAL', 'OP_ADD', 'OP_SUB', 'OP_MUL', 'OP_DIV', 'OP_MOD', 'OP_LSHIFT', 'OP_RSHIFT', 'OP_BOOLAND', 'OP_BOOLOR', 'OP_NUMEQUAL', 'OP_NUMEQUALVERIFY', 'OP_NUMNOTEQUAL', 'OP_LESSTHAN', 'OP_GREATERTHAN', 'OP_LESSTHANOREQUAL', 'OP_GREATERTHANOREQUAL', 'OP_MIN', 'OP_MAX', 'OP_WITHIN', 'OP_RIPEMD160', 'OP_SHA1', 'OP_SHA256', 'OP_HASH160', 'OP_HASH256', 'OP_CODESEPARATOR', 'OP_CHECKSIG', 'OP_CHECKSIGVERIFY', 'OP_CHECKMULTISIG', 'OP_CHECKMULTISIGVERIFY', 'OP_NOP1', 'OP_CHECKLOCKTIMEVERIFY', 'OP_CHECKSEQUENCEVERIFY', 'OP_NOP4', 'OP_NOP5', 'OP_NOP6', 'OP_NOP7', 'OP_NOP8', 'OP_NOP9', 'OP_NOP10'];
+  export const p2wpkh_script = (pubkeyhash : string) : string[] => ['OP_DUP','OP_HASH160',pubkeyhash,'OP_EQUALVERIFY','OP_CHECKSIG'];
   export const parse = (bin : Buffer, limit : number = 4294967295) : string[] => {
     limit -= 1;
     if (bin.length === 0 || limit < 0) return [];
@@ -79,19 +83,15 @@ namespace Script {
   };
   let top = (s : any[]) : any => s[s.length - 1];
   let secp256k1 = new elliptic.ec('secp256k1');
-  let ripemd = (buf : Buffer) : Buffer => new      ripemd160().update(buf).digest();
-  let sha256 = (buf : Buffer) : Buffer => createHash('sha256').update(buf).digest();
-  let sha1   = (buf : Buffer) : Buffer => createHash('sha1')  .update(buf).digest();
-  let verify = (pk : string, sig : string, subscr : string[], buildtx : (Buffer,number) => Buffer) : boolean => {
+  let verify = (pk : Buffer, sig : Buffer, msg : Buffer) : boolean => {
     try {
-      let pkk = secp256k1.keyFromPublic(pk,'hex'); let buf = Buffer.from(sig,'hex');
-      let txh = sha256(sha256(buildtx(assemble(subscr.filter(x => x !== 'OP_CODESEPARATOR')),buf[buf.length - 1])));
+      let pkk = secp256k1.keyFromPublic(pk);
       try {
-        if (buf[0] !== 48) throw new RangeError();
-        return pkk.verify(txh,buf.slice(0,2 + buf[1])) || pkk.verify(Buffer.from([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),buf.slice(0,2 + buf[1]));
+        if (sig[0] !== 48) throw new RangeError();
+        return pkk.verify(msg,sig.slice(0,2 + sig[1])) || pkk.verify(Buffer.from([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),sig.slice(0,2 + sig[1]));
       }
       catch (_) {
-        return pkk.verify(txh,buf.slice(0,-1))         || pkk.verify(Buffer.from([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),buf.slice(0,-1));
+        return pkk.verify(msg,sig.slice(0,-1))         || pkk.verify(Buffer.from([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),sig.slice(0,-1));
       }
     }
     catch (_) {
@@ -100,7 +100,7 @@ namespace Script {
   };
   class Stack {
     store : string[];
-    constructor() { this.store = []; }
+    constructor(init_store : string[] = []) { this.store = init_store; }
     length() : number { return this.store.length; }
     isempty() : boolean { return this.length() === 0; }
     popbool() : boolean { let x = this.pop(); return x !== '' && x !== '00' && x !== '80'; }
@@ -131,11 +131,11 @@ namespace Script {
       this.store.push(y);
     }
   };
-  export const run = (scriptsig : string[], scriptpubkey : string[], buildtx : (Buffer,number) => Buffer, debug? : boolean) : boolean => {
+  export const run = (scriptsig : string[], scriptpubkey : string[], buildtx : (Buffer,number) => Buffer, initstack : string[] = [], debug? : boolean) : boolean => {
     let result = true;
     let script = Array.from(scriptsig).concat(scriptpubkey);
     let subscr = Array.from(scriptpubkey);
-    let stack = new Stack();
+    let stack = new Stack(initstack);
     let altstack = new Stack();
     let if_level = 0;
     let if_stack : boolean[] = [];
@@ -185,24 +185,24 @@ namespace Script {
         case 'OP_SIZE': stack.push(Buffer.from(stack.top(),'hex').length.toString(16)); break;
         case 'OP_EQUAL': stack.push(stack.pop() === stack.pop()); break;
         case 'OP_EQUALVERIFY': script.unshift('OP_VERIFY'); script.unshift('OP_EQUAL'); break;
-        case 'OP_1ADD':                             stack.push(         stack.popnum() + 1);                              break;
-        case 'OP_1SUB':                             stack.push(         stack.popnum() - 1);                              break;
-        case 'OP_2MUL':                             stack.push(         stack.popnum() * 2);              result = false; break;
-        case 'OP_2DIV':                             stack.push(         stack.popnum() / 2);              result = false; break;
-        case 'OP_NEGATE':                           stack.push(       - stack.popnum());                                  break;
-        case 'OP_ABS':                              stack.push(Math.abs(stack.popnum()));                                 break;
-        case 'OP_NOT':                              stack.push(         stack.popnum() === 0);                            break;
-        case 'OP_0NOTEQUAL':                        stack.push(       !(stack.popnum() === 0));                           break;
-        case 'OP_ADD':                              stack.push(         stack.popnum() + stack.popnum());                 break;
-        case 'OP_SUB':                              stack.push(       - stack.popnum() + stack.popnum());                 break;
-        case 'OP_MUL':                              stack.push(         stack.popnum() * stack.popnum()); result = false; break;
-        case 'OP_DIV':    { let x = stack.popnum(); stack.push(         stack.popnum() /  x);             result = false; break; }
-        case 'OP_MOD':    { let x = stack.popnum(); stack.push(         stack.popnum() %  x);             result = false; break; }
-        case 'OP_LSHIFT': { let x = stack.popnum(); stack.push(         stack.popnum() << x);             result = false; break; }
-        case 'OP_RSHIFT': { let x = stack.popnum(); stack.push(         stack.popnum() >> x);             result = false; break; }
-        case 'OP_BOOLAND': { let x = stack.popbool(); stack.push(       stack.popbool() && x);                            break; }
-        case 'OP_BOOLOR':  { let x = stack.popbool(); stack.push(       stack.popbool() || x);                            break; }
-        case 'OP_NUMEQUAL':                         stack.push(         stack.popnum() === stack.popnum());               break;
+        case 'OP_1ADD':                               stack.push(         stack.popnum() + 1);                              break;
+        case 'OP_1SUB':                               stack.push(         stack.popnum() - 1);                              break;
+        case 'OP_2MUL':                               stack.push(         stack.popnum() * 2);              result = false; break;
+        case 'OP_2DIV':                               stack.push(         stack.popnum() / 2);              result = false; break;
+        case 'OP_NEGATE':                             stack.push(       - stack.popnum());                                  break;
+        case 'OP_ABS':                                stack.push(Math.abs(stack.popnum()));                                 break;
+        case 'OP_NOT':                                stack.push(         stack.popnum() === 0);                            break;
+        case 'OP_0NOTEQUAL':                          stack.push(       !(stack.popnum() === 0));                           break;
+        case 'OP_ADD':                                stack.push(         stack.popnum() + stack.popnum());                 break;
+        case 'OP_SUB':                                stack.push(       - stack.popnum() + stack.popnum());                 break;
+        case 'OP_MUL':                                stack.push(         stack.popnum() * stack.popnum()); result = false; break;
+        case 'OP_DIV':     { let x = stack.popnum();  stack.push(         stack.popnum() /  x);             result = false; break; }
+        case 'OP_MOD':     { let x = stack.popnum();  stack.push(         stack.popnum() %  x);             result = false; break; }
+        case 'OP_LSHIFT':  { let x = stack.popnum();  stack.push(         stack.popnum() << x);             result = false; break; }
+        case 'OP_RSHIFT':  { let x = stack.popnum();  stack.push(         stack.popnum() >> x);             result = false; break; }
+        case 'OP_BOOLAND': { let x = stack.popbool(); stack.push(         stack.popbool() && x);                            break; }
+        case 'OP_BOOLOR':  { let x = stack.popbool(); stack.push(         stack.popbool() || x);                            break; }
+        case 'OP_NUMEQUAL':                           stack.push(         stack.popnum() === stack.popnum());               break;
         case 'OP_NUMEQUALVERIFY': script.unshift('OP_VERIFY'); script.unshift('OP_NUMEQUAL'); break;
         case 'OP_NUMNOTEQUAL':        stack.push(stack.popnum() !== stack.popnum()); break;
         case 'OP_LESSTHAN':           stack.push(stack.popnum() >   stack.popnum()); break;
@@ -218,7 +218,11 @@ namespace Script {
         case 'OP_HASH160':   stack.push(ripemd(sha256(Buffer.from(stack.pop(),'hex'))).toString('hex')); break;
         case 'OP_HASH256':   stack.push(sha256(sha256(Buffer.from(stack.pop(),'hex'))).toString('hex')); break;
         case 'OP_CODESEPARATOR': subscr = Array.from(script); break;
-        case 'OP_CHECKSIG': { let pk = stack.pop(); let sig = stack.pop(); stack.push(verify(pk,sig,subscr,buildtx)); break; }
+        case 'OP_CHECKSIG': {
+          let pk = Buffer.from(stack.pop(),'hex'); let sig = Buffer.from(stack.pop(),'hex');
+          stack.push(verify(pk,sig,sha256(sha256(buildtx(assemble(subscr.filter(x => x !== 'OP_CODESEPARATOR')),sig[sig.length - 1])))));
+          break;
+        }
         case 'OP_CHECKSIGVERIFY': script.unshift('OP_VERIFY'); script.unshift('OP_CHECKSIG'); break;
         case 'OP_CHECKMULTISIG': {
           let M = stack.pop(); let pks = []; for (let i = 0 ; i < M ; i += 1) pks.push(stack.pop());
@@ -226,8 +230,9 @@ namespace Script {
           if (!stack.isempty() && stack.top() === '00') stack.pop();
           let n = 0;
           for (let sh of shs) {
+            let sig = Buffer.from(sh,'hex');
             for (let pk of pks) {
-              if (verify(pk,sh,subscr,buildtx)) {
+              if (verify(Buffer.from(pk,'hex'),sig,sha256(sha256(buildtx(assemble(subscr.filter(x => x !== 'OP_CODESEPARATOR')),sig[sig.length - 1]))))) {
                 n += 1;
                 break;
               }
@@ -252,7 +257,7 @@ namespace Script {
       }
       if (debug) console.log(result,script,stack,if_level,if_stack);
     }
-    if (!stack.isempty) result = result && stack.pop();
+    if (!stack.isempty()) result = result && stack.pop();
     if (result                                  &&
         !stack.isempty()                        &&
         scriptpubkey   .length === 3            &&
@@ -262,9 +267,13 @@ namespace Script {
         scriptsig.map(x => x.substr(0,2) !== 'OP').reduce((x,y) => x && y,true)) {
       let scriptsig2 = Array.from(scriptsig);
       let scriptpubkey2 = parse(Buffer.from(scriptsig2.pop(),'hex'));
-      result = run(scriptsig2,scriptpubkey2,buildtx,debug);
+      if (initstack.length) {
+        console.assert(scriptpubkey2.length === 2 && scriptpubkey2[0] === 'OP_FALSE');
+        scriptpubkey2 = p2wpkh_script(scriptpubkey2[1]);
+      }
+      result = run(scriptsig2,scriptpubkey2,buildtx,initstack,debug);
     }
-    if (!result && scriptsig.length > 0) result = run([],scriptpubkey,buildtx,debug);
+    if (!result && scriptsig.length) result = run([],scriptpubkey,buildtx,initstack,debug);
     return result;
   };
 };
@@ -338,7 +347,7 @@ namespace Transaction {
       [tx.vout[i].scriptPubKey.hex,bin] = Utils.parsefixlen(bin,len);
       tx.vout[i].scriptPubKey.asm = Script.parse(tx.vout[i].scriptPubKey.hex);
     }
-    tx.witness = tx.flag ? Array(vincnt) : [];
+    tx.witness = Array.from(Array(vincnt).keys()).map(_ => []);
     if (tx.flag) for (let i = 0 ; i < vincnt ; i += 1) {
       let len : number; [len,      bin] = Utils.parsevarint(bin);
       tx.witness[i] = Array(len);
@@ -351,6 +360,7 @@ namespace Transaction {
     if (bin.length) throw new Error("transaction parsing failed");
     return tx;
   };
+  let enclen = (buf : Buffer) : Buffer => Buffer.concat([varint(buf.length),buf]);
   export const assemble = (tx : parsed) : Buffer => {
     let bin : Buffer[] = [];
     bin.push(fixint(tx.version,4));
@@ -359,29 +369,49 @@ namespace Transaction {
     for (let vin of tx.vin) {
       bin.push(Utils.reverse(vin.txid));
       bin.push(       fixint(vin.vout,4));
-      bin.push(       varint(vin.scriptSig.hex.length));
-      bin.push(              vin.scriptSig.hex);
+      bin.push(       enclen(vin.scriptSig.hex));
       bin.push(       fixint(vin.sequence,4));
     }
     bin.push(varint(tx.vout.length));
     for (let vout of tx.vout) {
       bin.push(fixint(vout.value,8));
-      bin.push(varint(vout.scriptPubKey.hex.length));
-      bin.push(       vout.scriptPubKey.hex);
+      bin.push(enclen(vout.scriptPubKey.hex));
     }
     if (tx.flag) for (let witness of tx.witness) {
       bin.push(varint(witness.length));
-      for (let item of witness) {
-        bin.push(varint(item.length));
-        bin.push(       item);
-      }
+      for (let item of witness) bin.push(enclen(item));
     }
     bin.push(fixint(tx.locktime,4));
     return Buffer.concat(bin);
   };
   export const verify = (tx : parsed, from : parsed[], debug? : boolean) : boolean => {
     for (let i = 0 ; i < tx.vin.length ; i += 1) {
-      if (!Script.run(tx.vin[i].scriptSig.asm,from[i].vout[tx.vin[i].vout].scriptPubKey.asm,(subscr,hashtype) => {
+      let scriptSig = tx.vin[i].scriptSig.asm;
+      let scriptPubKey = from[i].vout[tx.vin[i].vout].scriptPubKey.asm;
+      let initStack = tx.witness[i].map(x => x.toString('hex'));
+      let buildtx : (Buffer,number) => Buffer;
+           if (tx.flag && scriptPubKey.length === 3 && scriptPubKey[0] === 'OP_HASH160' && scriptPubKey[1].length === 40 && scriptPubKey[2] === 'OP_EQUAL') {
+        console.assert(scriptSig.length === 1);
+        console.assert(tx.witness[i].length === 2);
+        buildtx = (_,hashtype) => {
+          let bin : Buffer[] = [];
+          bin.push(fixint(tx.version,4));                                                                                                      // 1. nVersion of the transaction (4-byte little endian)
+          bin.push(sha256(sha256(Buffer.concat(tx.vin.map(vin => Buffer.concat([Utils.reverse(      vin.txid),fixint(      vin.vout,4)])))))); // 2. hashPrevouts (32-byte hash)
+          bin.push(sha256(sha256(Buffer.concat(tx.vin.map(vin => fixint(vin.sequence,4))))));                                                  // 3. hashSequence (32-byte hash)
+          bin.push(                                              Buffer.concat([Utils.reverse(tx.vin[i].txid),fixint(tx.vin[i].vout,4)]));     // 4. outpoint (32-byte hash + 4-byte little endian)
+          let scriptCode = Script.parse(Buffer.from(scriptSig[0],'hex'));                                                                      // 5. scriptCode of the input (serialized as scripts inside CTxOuts)
+          console.assert(scriptCode.length === 2 && scriptCode[0] === 'OP_FALSE' && scriptCode[1].length === 40);
+          bin.push(enclen(Script.assemble(Script.p2wpkh_script(scriptCode[1]))));
+          bin.push(fixint(from[i].vout[tx.vin[i].vout].value,8));                                                                              // 6. value of the output spent by this input (8-byte little endian)
+          bin.push(fixint(tx.vin[i].sequence,4));                                                                                              // 7. nSequence of the input (4-byte little endian)
+          bin.push(sha256(sha256(Buffer.concat(tx.vout.map(vout => Buffer.concat([fixint(vout.value,8),enclen(vout.scriptPubKey.hex)]))))));   // 8. hashOutputs (32-byte hash)
+          bin.push(fixint(tx.locktime,4));                                                                                                     // 9. nLocktime of the transaction (4-byte little endian)
+          bin.push(fixint(hashtype,4));                                                                                                        // 10. sighash type of the signature (4-byte little endian)
+          if (debug) console.log(JSON.stringify(bin,(key,value) => (value.type === 'Buffer') ? Buffer.from(value).toString('hex') : value,2));
+          return Buffer.concat(bin);
+        };
+      }
+      else buildtx = (subscr,hashtype) => {
         let t = parse(assemble(tx),true);
         t.vin[i].scriptSig.hex =              subscr;
         t.vin[i].scriptSig.asm = Script.parse(subscr);
@@ -402,7 +432,8 @@ namespace Transaction {
         if (hashtype & 128) t.vin = t.vin.slice(i,i + 1);
         if (debug) console.log(JSON.stringify(t,(key,value) => (value.type === 'Buffer') ? Buffer.from(value).toString('hex') : value,2));
         return Buffer.concat([assemble(t),fixint(hashtype,4)]);
-      },debug)) return false;
+      };
+      if (!Script.run(scriptSig,scriptPubKey,buildtx,initStack,debug)) return false;
     }
     return true;
   };
@@ -410,7 +441,7 @@ namespace Transaction {
 
 let btclient = new bitcoincore({ username: 'chelpis', password: 'chelpis' });
 let main = async () => {
-  for (let i = 289640 ; ; i += 1) {
+  for (let i = 481824 ; ; i += 1) {
     let block = await btclient.getBlock(await btclient.getBlockHash(i));
     for (let j = 1 ; j < block.tx.length ; j += 1) {
       console.log(i,j);
